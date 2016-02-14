2d7f1a23a98e5d6dcefa47d95f8860a39cf2d61f
alvarotrapote <alvaro.trapote@gmail.com>
2016-02-14 01:10:38 +0000

    Fix text colour changing based on background colour

--- js/app.js
+++ js/app.js
@@ -29,25 +29,28 @@ app.controller('MainCtrl', function($scope, $timeout, dateFilter) {
         var now = moment();
         var numberOfColours = 256 * 256 * 256;
         // Difference btw now and the start of the year, in milliseconds. Then, map the number of colours with the number of seconds in a year.
-        if (secondsInYear > numberOfColours) {
-            $scope.thecolour = Math.floor(now.diff(startOfYear, 'seconds') - numberOfColours).toString(16);
+        if (now.diff(startOfYear, 'seconds') < numberOfColours) {
+            $scope.thecolour = Math.floor(now.diff(startOfYear, 'seconds')).toString(16);            
         } else {
-            $scope.thecolour = Math.floor(now.diff(startOfYear, 'seconds')).toString(16);
+            $scope.thecolour = Math.floor(now.diff(startOfYear, 'seconds') - numberOfColours).toString(16);
         }
-        $scope.foregroundColor = $scope.idealTextColor($scope.thecolour);
+        $scope.foregroundColor = $scope.getTextColor($scope.thecolour);
     };
 
-    $scope.idealTextColor = function(bgColor) {
+    $scope.getTextColor = function(bgColor) {
         var nThreshold = 105;
         var components = $scope.getRGBComponents(bgColor);
+        // debugger
         var bgDelta = (components.R * 0.299) + (components.G * 0.587) + (components.B * 0.114);
+        // debugger
+        console.log((255 - bgDelta) < nThreshold)
         return ((255 - bgDelta) < nThreshold) ? "#000000" : "#ffffff";
     }
 
     $scope.getRGBComponents = function(color) {
-        var r = color.substring(1, 3);
-        var g = color.substring(3, 5);
-        var b = color.substring(5, 7);
+        var r = color.substring(0, 2);
+        var g = color.substring(2, 4);
+        var b = color.substring(4, 6);
         return {
             R: parseInt(r, 16),
             G: parseInt(g, 16),
